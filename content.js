ce937b16704f0b73d89bd79e10bf8be210cf02fe
Kaoru Kawabata / KijiroraFinch <KijitoraFinch-caffeine@proton.me>
2024-03-27 20:09:38 +0900

    Revise dict.json and add exluding function

--- content.js
+++ content.js
@@ -1,14 +1,20 @@
-// content.js
-function replaceAllText(element, dict) {
+console.log('content.js start')
+async function sha256(text){
+    const uint8  = new TextEncoder().encode(text)
+    const digest = await crypto.subtle.digest('SHA-256', uint8)
+    return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2,'0')).join('')
+}
+
+async function replaceAllText(element, dict, exceptions) {
     if (element.hasChildNodes()) {
-        Array.from(element.childNodes).forEach(childNode => replaceAllText(childNode, dict)); // Fix the function call inside the forEach loop
+        Array.from(element.childNodes).forEach(childNode => replaceAllText(childNode, dict, exceptions)); // Fix the function call inside the forEach loop
     } else if (element.nodeType === Text.TEXT_NODE) {
         // Replace the text of the text node
-        element.textContent = replaceTextByDict(element.textContent, dict);
+        element.textContent = await replaceTextByDict(element.textContent, dict, exceptions);
     }
 }
-// targetDictのkey-valueを反転させ、重複があれば最初のものを使用
 
+// targetDictのkey-valueを反転させ、重複があれば最初のものを使用
 const reverseDict = targetDict => {
     const reversedDict = {};
     for (let key in targetDict) {
@@ -17,7 +23,6 @@ const reverseDict = targetDict => {
             for (let i = 0; i < value.length; i++) {
                 if (!(value[i] in reversedDict)) {
                     reversedDict[value[i]] = key;
-                    break;
                 }
             }
         } else {
@@ -28,16 +33,35 @@ const reverseDict = targetDict => {
     }
     return reversedDict;
 }
-function replaceTextByDict(text, dict){
-    let result = text; // Add missing variable declaration
-    for (let key in dict){
-        result = result.replace(key, dict[key])
-    }
+
+
+async function replaceTextByDict(text, dict, exceptions) {
+    let result = text;
+    let exdict = {};
+
+    for (let exception of exceptions){
+        const hashed = await sha256(exception);
+        exdict[hashed] = exception;
+        console.log("hash is "+hashed);
+        result = result.replaceAll(exception, hashed);
+    };
+    console.log("current result === "+result)
+
+    Object.keys(dict).forEach((key) => {
+        const value = dict[key];
+        result = result.replaceAll(key, value);
+    })
+
+    Object.keys(exdict).forEach((hashed) => {
+        const value = exdict[hashed];
+        result = result.replaceAll(hashed, value);
+    });
+    console.log("finally result === "+result)
     return result;
 }
 
-async function loadJson() {
-    const url = browser.runtime.getURL('dict.json');
+async function loadJson(jsonFile) {
+    const url = browser.runtime.getURL(jsonFile);
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
@@ -45,10 +69,17 @@ async function loadJson() {
 }
 
 async function main() {
-    const targetDict = await loadJson();
-    replaceAllText(document.body, reverseDict(targetDict));
+    console.log('start')
+    const targetDict = await loadJson('dict.json');
+    const defaultExceptionsJson = await loadJson('exceptions.exsample.json');
+    // "true"がvalueのものをリストとする
+    const exceptions = Object.keys(defaultExceptionsJson).filter((key) => defaultExceptionsJson[key]);
+    console.log(exceptions);
+    await replaceAllText(document.body, reverseDict(targetDict), exceptions);
     console.log('replece done!')
     console.log(reverseDict(targetDict))
 }
+
+console.log('content.js')
 
 main();
